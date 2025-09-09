d2f7d35c297027372e3c475c6b3a4c31659101c8
TatiRoque <tatiana@roque.com.ar>
2025-09-09 15:24:22 -0300

    Adapter add

---
+++ Adapter/Adapter/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Adapter
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            IPrinter printer = new ThermalAdapter(new OldThermalPrinter());
+            printer.Print("Adapter it´s working");
+        }
+    }
+}
